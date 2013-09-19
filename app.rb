09faa428fac6bcc8017db6f5bd0ea6068ca21a19
Omar Diab <o.s.diab@gmail.com>
2013-09-19 18:25:54 -0400

    added routes

--- app.rb
+++ app.rb
@@ -10,3 +10,31 @@ end
 get '/' do
   haml :index
 end
+
+get '/learn' do
+  haml :learn
+end
+
+get '/discuss' do
+  haml :discuss
+end
+
+get '/events' do
+  haml :events
+end
+
+get '/act' do
+  haml :act
+end
+
+get '/about' do
+  haml :about
+end
+
+get '/contact' do
+  haml :contact
+end
+
+get '/about_site' do
+  haml :'about_site'
+end
