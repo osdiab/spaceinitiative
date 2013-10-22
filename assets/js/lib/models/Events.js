adf75dcba4d56b261f5b770f5975fac7d8eb3820
Omar Diab <o.s.diab@gmail.com>
2013-10-22 00:27:48 -0700

    added basic events loading functionality

--- assets/js/lib/models/Events.js
+++ assets/js/lib/models/Events.js
@@ -2,19 +2,43 @@ function Events(feed) {
   this.feed = feed
 }
 
+Events.GCAL_TIME_FORMAT = 'YYYY-MM-DDThh:mm:ss.SSSZ'
+Events.START_OUTPUT_FORMAT = 'dddd MMM DD YYYY, h:mm a'
+Events.END_OUTPUT_FORMAT = 'h:mm a'
+
 Events.prototype.displayFeed = function (div)
 {
-  toAdd = document.createElement('ul')
-  $(toAdd).attr('class', 'eventList')
+  var eventList = document.createElement('ul')
+  $(eventList).attr('class', 'eventList')
 
-  $(this.feed).each(function(index, element) {
-    element = document.createElement('li')
-    $(element).attr('class', 'eventElem')
+  // for each event, create the event element
+  $(this.feed).each(function(index, event) {
+    var listElem = document.createElement('li')
+    $(listElem).attr('class', 'eventElem')
 
-    // TODO: fill element with data
+    var heading = document.createElement('h2')
+    $(heading).attr('class', 'eventHdr')
+    $(heading).text(event.title.$t)
 
-    $(toAdd).append(element)
+    var description = document.createElement('p')
+    $(description).attr('class', 'eventTime')
+
+    // calculate start time
+    var startTime = moment(event.gd$when[0].startTime, Events.GCAL_TIME_FORMAT)
+    startTime = startTime.format(Events.START_OUTPUT_FORMAT)
+
+    // calculate end time
+    var endTime = moment(event.gd$when[0].endTime, Events.GCAL_TIME_FORMAT)
+    endTime = endTime.format(Events.END_OUTPUT_FORMAT)
+
+    $(description).text(startTime + ' to ' + endTime)
+
+    // append generated elements
+    $(listElem).append(heading)
+    $(listElem).append(description)
+    $(eventList).append(listElem)
   })
 
-  $(div).append(toAdd)
+  // set div contents to contain generated event list
+  $(div).append(eventList)
 }
--- assets/js/lib/pages/IndexGet.js
+++ assets/js/lib/pages/IndexGet.js
@@ -1,15 +1,25 @@
 // URI for google calendar JSONP request.
-var GCAL_JSONP_CALLBACK = 'IndexGet.displayEvents'
-var GCAL_MAX_RESULTS = 5;
-var GCAL_URI = 'http://www.google.com/calendar/feeds/5or10qu0uhtfqcdqb3knrpn3r8%40group.calendar.google.com/public/full?alt=json-in-script&callback=' + GCAL_JSONP_CALLBACK + '&orderby=starttime&max-results=' + GCAL_MAX_RESULTS + '&singleevents=true&sortorder=ascending&futureevents=true'
 
 var IndexGet = new function() {
 }
 
+IndexGet.GCAL_JSONP_CALLBACK = 'IndexGet.displayEvents'
+IndexGet.GCAL_MAX_RESULTS = 5
+IndexGet.GCAL_URI = [
+    'http://www.google.com/calendar/feeds/',
+    '5or10qu0uhtfqcdqb3knrpn3r8%40group.calendar.google.com',
+    '/public/full?alt=json-in-script&callback=',
+    IndexGet.GCAL_JSONP_CALLBACK,
+    '&orderby=starttime&max-results=',
+    IndexGet.GCAL_MAX_RESULTS,
+    '&singleevents=true&sortorder=ascending&futureevents=true'
+  ].join('')
+
+
 IndexGet.init = function() {
   // launch jsonp request
   var gcalJsonpElem = document.createElement('script')
-  gcalJsonpElem.src = GCAL_URI
+  gcalJsonpElem.src = IndexGet.GCAL_URI
   $('#head').append(gcalJsonpElem)
 }
 
