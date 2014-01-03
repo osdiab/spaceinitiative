e77b335b59094069772b0891292aad587b850085
Omar Diab <o.s.diab@gmail.com>
2014-01-02 18:11:19 -0800

    feature(members): add members api endpoint, handle members in client

--- routes/api.rb
+++ routes/api.rb
@@ -10,4 +10,20 @@ class App < Sinatra::Base
     posts = feed.get(params[:num_posts])
     posts.to_json
   end
+
+  get '/api/members.json' do
+    require_lib 'models/Member'
+    content_type :json
+    members = Member.all
+    payload = []
+    members.each do |member|
+      payload << { name: member.name,
+                   id: member.id,
+                   title: member.title,
+                   image: member.image,
+                   thumb: member.thumb,
+                   bio: member.bio }
+    end
+    payload.to_json
+  end
 end
