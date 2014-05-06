c114a0317aee9ce580def79207d358fe24d77483
Omar Diab <o.s.diab@gmail.com>
2014-05-06 00:03:47 -0700

    add rails project, proper db settings

---
+++ spaceinitiative/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spaceinitiative::Application.config.secret_key_base = '026331f31a3da14176c4159976b5dedf0849eb14655362dc13f3f6cb7596d8c78776927fc5f7ad1d46b12dfd7f5a7aaac28d843935468d8964dd1bb977529c47'
